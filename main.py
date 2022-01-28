fd5e628e0c4363c305af8a6fdbf70c2e2b9aef1d
alphadzingina <alphaadx6@gmail.com>
2022-01-28 03:08:15 +0100

    find highest bidder

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ from art import logo
 
 print(logo)
 
+def find_highest_bidder(bidding_record):
+  highest_bid = 0
+  winner = ""
+  # bidding_record = {"Angela": 123, "James": 321}
+  for bidder in bidding_record:
+    bid_amount = bidding_record[bidder]
+    if bid_amount > highest_bid: 
+      highest_bid = bid_amount
+      winner = bidder
+  
+  print(bids)
+  print(f"The winner is {winner} with a bid of ${highest_bid}")
+
 bids = {}
 more_bidders = True
 
@@ -15,7 +28,6 @@ while more_bidders == True:
     response = input("Are there more bidders? Type 'yes' or 'no': ")
     if response == "no":
         more_bidders = False
+        find_highest_bidder(bids)
     elif response == "yes":
         os.system('cls||clear')
-
-print(bids)
